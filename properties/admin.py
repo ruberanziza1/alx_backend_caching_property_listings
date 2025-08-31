19e33482790b58e2e57868b8651a882f1509a268
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-31 12:47:36 +0200

    Add files via upload

--- properties/admin.py
+++ properties/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Property
 
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'location', 'created_at')
-    list_filter = ('location', 'created_at')
-    search_fields = ('title', 'description', 'location')
-    ordering = ('-created_at',)
+# Register your models here.
--- properties/models.py
+++ properties/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Property(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -9,6 +10,3 @@ class Property(models.Model):
 
     def __str__(self):
         return self.title
-
-    class Meta:
-        verbose_name_plural = "Properties"
