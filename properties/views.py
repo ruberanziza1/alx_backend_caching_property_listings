055802546c97662a587047c9a1898484a35375a7
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-31 13:20:59 +0200

    Delete properties directory

--- properties/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from django.views.decorators.cache import cache_page
-from .utils import get_all_properties
-
-# Cache the entire view for 15 minutes
-@cache_page(60 * 15)
-def property_list(request):
-    properties = get_all_properties()
-    return JsonResponse({"data": properties}, safe=False)
