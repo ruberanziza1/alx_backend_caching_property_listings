d11f6e150084a935a19dc20fb988cf8d449b92df
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-31 19:47:18 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from .models import Property
 from django.views.decorators.cache import cache_page
-from .utils import get_all_properties, get_redis_cache_metrics
+from .utils import get_all_properties
 
-@cache_page(60 * 15)
+@cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
-    properties_queryset = get_all_properties()
-    return JsonResponse({'data': list(properties_queryset)})
-
-def cache_metrics_view(request):
-    metrics = get_redis_cache_metrics()
-    return JsonResponse(metrics)
+    properties = get_all_properties()
+    context = {'properties': properties}
+    return render(request, 'properties/property_list.html', context)
