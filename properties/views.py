55ce1722342a30f50761d770f676a2ec8d97115f
ruberanziza1 <janvier.ruberanziza@gmail.com>
2025-08-31 16:25:22 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from .utils import get_all_properties
 from django.views.decorators.cache import cache_page
-from .models import Property
 
-@cache_page(60 * 15)  # Cache for 15 minutes
+@cache_page(60 * 15)  # Cache the view for 15 minutes
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     context = {
         'properties': properties
     }
